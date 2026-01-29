8b401752590bb332112d2b5ae2570bf8b765a501
hslee <hslee>
2026-01-29 22:55:29 +0900

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,13 @@
 #include "v001/E10_EliteAirMouse_004.h"
 
 
-// 에어마우스 객체 생성
-EliteAirMouse airMouse;
+CL_E10_EliteAirMouse g_E10_airMouse;
 
-/**
- * @brief 시스템 설정 및 에어마우스 가동
- */
 void setup() {
-    // 에어마우스 모듈 시작 (내부적으로 멀티코어 태스크 생성)
-    airMouse.begin();
+    Serial.begin(115200);
+    g_E10_airMouse.begin();
 }
 
-/**
- * @brief 메인 루프는 비워두어 자원을 최소화 (모든 로직은 태스크에서 동작)
- */
 void loop() {
-    // FreeRTOS가 태스크를 관리하므로 메인 루프 태스크는 삭제하여 메모리 확보 가능
-    vTaskDelete(NULL); 
+    vTaskDelete(NULL);
 }
