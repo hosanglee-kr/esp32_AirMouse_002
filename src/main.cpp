5f52a56d048dd64528e3b0bc1aadc38059cb369a
hslee <hslee>
2026-01-31 10:12:54 +0900

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 // ======================================================
 #include <Arduino.h>
 
-#include "v001/C10_Config_010.h"
-#include "v001/E10_EliteAirMouse_012.h"
-#include "v001/W10_WebConfig_010.h"
+#include "v010/C10_Config_010.h"
+#include "v010/E10_EliteAirMouse_012.h"
+#include "v010/W10_WebConfig_010.h"
 
 CL_C10_Config        g_c10;
 CL_E10_EliteAirMouse g_e10;
