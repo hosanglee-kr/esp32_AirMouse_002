0fdbc4f1829d6b29b0632ef478a7626c4439d09b
hslee <hslee>
2026-02-02 21:48:29 +0900

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 // ======================================================
 #include <Arduino.h>
 
+
+#include "A40_ComFunc_070.h"   // 내부에서 D10_Logger_060.h 포함
+
 #include "v010/C10_Config_023.h"
 #include "v010/E10_EliteAirMouse_023.h"
 #include "v010/W10_WebConfig_023.h"
@@ -12,6 +15,19 @@ CL_E10_EliteAirMouse g_e10;
 CL_W10_WebConfig     g_w10;
 
 void setup() {
+  CL_D10_Logger::begin(Serial);
+  CL_D10_Logger::setLevel(EN_D10_LOG_INFO);
+  CL_D10_Logger::enableTimestamp(true);
+  CL_D10_Logger::enableMemUsage(false);
+
+  // LittleFS
+  if (!LittleFS.begin(true)) {
+    D10_LOGE("LittleFS begin failed");
+  } else {
+    D10_LOGI("LittleFS mounted");
+  }
+  
+  
   g_c10.begin(true);
   g_e10.begin(&g_c10);
 
