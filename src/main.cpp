514ad681677d6a7c57bdca683e4a01596545f854
hslee <hslee>
2026-02-04 21:06:10 +0900

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #include "v010/A40_ComFunc_070.h"   // 내부에서 D10_Logger_060.h 포함
 
-#include "v010/C10_Config_023.h"
+#include "v010/C10_Config_025.h"
 #include "v010/E10_EliteAirMouse_025.h"
 #include "v010/W10_WebConfig_026.h"
 
